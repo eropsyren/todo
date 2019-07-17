ed8897bb626ec2a5b33216db1d30fb9b551e5c55
eropsyren <eropsyren@gmail.com>
2019-07-17 23:32:02 +0200

    rework init function

--- src/init.rs
+++ src/init.rs
@@ -1,20 +1,12 @@
 use crate::constants::TODO_FILE_NAME;
-use crate::utils;
 use std::error::Error;
 use std::fs::File;
-use std::path::Path;
 
 pub fn init() {
-    let already_exists = Path::new(TODO_FILE_NAME).exists();
+    let f = File::create(TODO_FILE_NAME);
 
-    if already_exists {
-        eprintln!("error: there is a .todo file already");
-    } else {
-        let f = File::create(".todo");
-
-        match f {
-            Ok(_) => (),
-            Err(error) => eprintln!("error generating .todo file: {}", error.description()),
-        }
+    match f {
+        Ok(_) => (),
+        Err(error) => eprintln!("error generating .todo file: {}", error.description()),
     }
 }
