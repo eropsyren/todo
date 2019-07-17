fc6ae03eed703369427c298a88806fc98b4485e8
eropsyren <eropsyren@gmail.com>
2019-07-17 23:05:51 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ fn main() {
     let matches = App::new(clap::crate_name!())
         .version(clap::crate_version!())
         .author(clap::crate_authors!())
-        .subcommand(SubCommand::with_name("init").about("initialize todo list in the current directory"))
+        .subcommand(
+            SubCommand::with_name("init").about("initialize todo list in the current directory"),
+        )
         .subcommand(SubCommand::with_name("add").about("add a task to todo list"))
         .subcommand(SubCommand::with_name("list").about("list all tasks in todo list"))
         .get_matches();
