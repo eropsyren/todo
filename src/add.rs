2e08d7f7b0b808b04f128ef98d24951d8c09d962
eropsyren <eropsyren@gmail.com>
2019-07-20 19:48:21 +0200

    add simple add reimplementation using json crate

--- src/add.rs
+++ src/add.rs
@@ -1,27 +1,54 @@
 use crate::constants::TODO_FILE_NAME;
-use std::error::Error;
-use std::fs::OpenOptions;
-use std::io::Write;
+use json::{self, JsonValue};
+use std::fs;
+use std::fs::File;
 
 pub fn add(task: &str) {
-    let file = OpenOptions::new()
-        .read(true)
-        .append(true)
-        .open(TODO_FILE_NAME);
+    let file = match fs::read_to_string(TODO_FILE_NAME) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("error reading file {}: {}", TODO_FILE_NAME, err);
 
-    match file {
-        Ok(mut file) => match write!(&mut file, "{}", task) {
+            return;
+        }
+    };
+
+    let tasks = match json::parse(&file) {
+        Ok(json) => json,
+        Err(err) => {
+            eprintln!("error parsing file {} as json: {}", TODO_FILE_NAME, err);
+
+            return;
+        }
+    };
+
+    let mut tasks = match tasks {
+        JsonValue::Array(_) => tasks,
+        _ => {
+            eprintln!("file {} is not a json array", TODO_FILE_NAME);
+
+            return;
+        }
+    };
+
+    let new_task = json::object!{
+        "msg" => task,
+    };
+
+    match tasks.push(new_task) {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("error pushing to json array: {}", err);
+
+            return;
+        }
+    }
+
+    match File::create(TODO_FILE_NAME) {
+        Ok(mut file) => match tasks.write(&mut file) {
             Ok(_) => (),
-            Err(err) => eprintln!(
-                "error writing task to {}: {}",
-                TODO_FILE_NAME,
-                err.description()
-            ),
+            Err(err) => eprintln!("error writing to {}: {}", TODO_FILE_NAME, err),
         },
-        Err(err) => eprintln!(
-            "error reading file {}: {}",
-            TODO_FILE_NAME,
-            err.description()
-        ),
+        Err(err) => eprintln!("error rewriting {}: {}", TODO_FILE_NAME, err),
     }
 }
