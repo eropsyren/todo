abd5a1d49f6d2a10ef98316b7d83332aabb608b0
eropsyren <eropsyren@gmail.com>
2019-07-20 20:18:23 +0200

    rework list subcommand to use json crate

--- src/list.rs
+++ src/list.rs
@@ -1,22 +1,37 @@
 use crate::constants::TODO_FILE_NAME;
 use colored::Colorize;
-use std::error::Error;
+use json::{self, JsonValue};
 use std::fs;
 
 pub fn list() {
-    let content = fs::read_to_string(TODO_FILE_NAME);
+    let file = match fs::read_to_string(TODO_FILE_NAME) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("error reading file {}: {}", TODO_FILE_NAME, err);
 
-    match content {
-        Ok(content) => {
-            for (idx, line) in content.lines().enumerate() {
-                let idx = format!("[{}]", idx);
-                println!("{} {}", idx.cyan(), line.yellow());
-            }
+            return;
         }
-        Err(err) => eprintln!(
-            "error reading file {}: {}",
-            TODO_FILE_NAME,
-            err.description()
-        ),
+    };
+
+    let tasks = match json::parse(&file) {
+        Ok(json) => json,
+        Err(err) => {
+            eprintln!("error parsing file {} as json: {}", TODO_FILE_NAME, err);
+
+            return;
+        }
+    };
+
+    let tasks = match tasks {
+        JsonValue::Array(tasks) => tasks,
+        _ => {
+            eprintln!("error parsing file {}: not a json array", TODO_FILE_NAME);
+
+            return;
+        }
+    };
+
+    for task in tasks {
+        println!("{}", task["msg"].to_string().yellow());
     }
 }
