aeef84f0d6c12bd1df0717d93c14bc5ce7c64b2c
eropsyren <eropsyren@gmail.com>
2019-07-21 19:51:13 +0200

    add print_error macro

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,19 @@
 use crate::constants::TODO_FILE_NAME;
 use std::path::Path;
 
+macro_rules! print_error {
+    ($($arg:tt)*) => {
+        eprintln!("{}", colored::Colorize::red(std::fmt::format(format_args!($($arg)*)).as_str()));
+    }
+}
+
 pub fn if_todo_exists(f: impl FnOnce() -> ()) {
     let exists = Path::new(TODO_FILE_NAME).exists();
 
     if exists {
         f();
     } else {
-        eprintln!("error: there is no .todo file");
+        print_error!("error: there is no .todo file");
     }
 }
 
@@ -15,7 +21,7 @@ pub fn if_todo_not_exists(f: impl FnOnce() -> ()) {
     let exists = Path::new(TODO_FILE_NAME).exists();
 
     if exists {
-        eprintln!("error: .todo file exists already");
+        print_error!("error: .todo file exists already");
     } else {
         f();
     }
