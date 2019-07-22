16ea07bd70567815171a0155c77ac35ca9432364
eropsyren <eropsyren@gmail.com>
2019-07-22 19:11:01 +0200

    improve done subcommand code

--- src/done.rs
+++ src/done.rs
@@ -6,19 +6,19 @@ pub fn done(id: &str) {
     let tasks = get_json_from_file_or_return!(TODO_FILE_NAME);
     let mut tasks = validate_json_or_return!(tasks, TODO_FILE_NAME);
 
-    for (task_id, task) in tasks.entries_mut() {
-        if id == task_id {
-            task[STATUS] = JsonValue::String(String::from(DONE));
-
-            match File::create(TODO_FILE_NAME) {
-                Ok(mut file) => match tasks.write(&mut file) {
-                    Ok(_) => (),
-                    Err(err) => print_error!("error writing to {}: {}", TODO_FILE_NAME, err),
-                },
-                Err(err) => print_error!("error rewriting {}: {}", TODO_FILE_NAME, err),
-            }
-
+    match &mut tasks[id] {
+        JsonValue::Null => {
+            print_error!("error: no task with {} id", id);
             return;
         }
+        task => task[STATUS] = JsonValue::String(String::from(DONE)),
+    }
+
+    match File::create(TODO_FILE_NAME) {
+        Ok(mut file) => match tasks.write(&mut file) {
+            Ok(_) => (),
+            Err(err) => print_error!("error writing to {}: {}", TODO_FILE_NAME, err),
+        },
+        Err(err) => print_error!("error rewriting {}: {}", TODO_FILE_NAME, err),
     }
 }
