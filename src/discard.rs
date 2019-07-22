cf9c8431700a0b4ea392de68bca3a6bbbbc1d45c
eropsyren <eropsyren@gmail.com>
2019-07-22 20:25:22 +0200

    discarded or done tasks cannot be marked as discarded

--- src/discard.rs
+++ src/discard.rs
@@ -1,18 +1,36 @@
-use crate::constants::{DISCARDED, STATUS, TODO_FILE_NAME};
+use crate::constants::{DISCARDED, DONE, STATUS, TODO_FILE_NAME};
 use json::JsonValue;
 
 pub fn discard(id: &str) {
     let tasks = get_json_from_file_or_return!(TODO_FILE_NAME);
     let mut tasks = validate_json_or_return!(tasks, TODO_FILE_NAME);
 
-    match &mut tasks[id] {
+    let task: &mut JsonValue = match &mut tasks[id] {
         JsonValue::Null => {
             print_error!("error: no task with {} id", id);
 
             return;
         }
-        task => task[STATUS] = JsonValue::String(String::from(DISCARDED)),
+        task => task,
+    };
+
+    if let JsonValue::Short(s) = task[STATUS] {
+        match s.as_str() {
+            DONE => {
+                print_error!("error: cannot discard a {} task", DONE);
+
+                return;
+            }
+            DISCARDED => {
+                print_error!("error: cannot discard a {} task", DISCARDED);
+
+                return;
+            }
+            _ => (),
+        }
     }
+
+    task[STATUS] = JsonValue::String(String::from(DISCARDED));
 
     write_json_to_file_or_err!(tasks, TODO_FILE_NAME);
 }
