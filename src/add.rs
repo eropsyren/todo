f689bfa6cfaa834dd5afad5b50897e3c99920059
eropsyren <eropsyren@gmail.com>
2019-07-23 19:09:07 +0200

    slim down add_subtask function's code

--- src/add.rs
+++ src/add.rs
@@ -34,8 +34,7 @@ pub fn add_subtask(id: &str, task: &str) {
     let tasks = get_json_from_file_or_return!(TODO_FILE_NAME);
     let mut tasks = validate_json_or_return!(tasks, TODO_FILE_NAME);
     let super_task = get_prop_or_return!(tasks, id, JsonValue::Object);
-    let super_task_msg = extract_prop_or_return!(super_task, MESSAGE, JsonValue::Short);
-    let super_task_msg = super_task_msg.as_str();
+    let super_task_msg = get_prop_or_return!(super_task, MESSAGE, JsonValue::Short).to_string();
     let task = task.trim();
     let task_hash = hash(&format!("{}\n{}", super_task_msg, task));
 
@@ -46,7 +45,7 @@ pub fn add_subtask(id: &str, task: &str) {
     let sub_tasks = get_prop_or_return!(super_task, SUBTASKS, JsonValue::Object);
 
     if sub_tasks.has_key(&task_hash) {
-        print_error!("error: subtask '{}' already present", task);
+        print_error!("error: subtask '{}' is present already", task);
 
         return;
     }
