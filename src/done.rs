73f9573e1e011d85713877f57105de70db1b5208
eropsyren <eropsyren@gmail.com>
2019-07-24 18:44:57 +0200

    add subtasks handling to done command

--- src/done.rs
+++ src/done.rs
@@ -1,17 +1,38 @@
-use crate::constants::{DONE, STATUS, TODO_FILE_NAME};
+use crate::constants::{DISCARDED, DONE, STATUS, SUBTASKS, TODO_FILE_NAME, UNDONE};
 use json::JsonValue;
+use std::process;
 
 pub fn done(id: &str) {
     let tasks = get_json_from_file_or_exit!(TODO_FILE_NAME);
     let mut tasks = is_object_or_exit!(tasks, TODO_FILE_NAME);
 
-    match &mut tasks[id] {
-        JsonValue::Null => {
-            print_error!("error: no task with {} id", id);
+    if tasks.has_key(id) {
+        for (_, task) in tasks[id][SUBTASKS].entries() {
+            let status =
+                get_prop_or_exit!(task, STATUS, JsonValue::Short, JsonValue::String).to_string();
 
-            return;
+            match status.as_str() {
+                DONE | DISCARDED => (),
+                UNDONE => {
+                    print_error!("error: one of the subtasks is {}", UNDONE);
+                    process::exit(1);
+                }
+                val => {
+                    print_error!("error: invalid status '{}'", val);
+                    process::exit(1);
+                }
+            }
         }
-        task => task[STATUS] = JsonValue::String(String::from(DONE)),
+
+        tasks[id][STATUS] = JsonValue::String(String::from(DONE));
+    } else {
+        for (_, task) in tasks.entries_mut() {
+            if task[SUBTASKS].has_key(id) {
+                task[SUBTASKS][id][STATUS] = JsonValue::String(String::from(DONE));
+
+                break;
+            }
+        }
     }
 
     write_json_to_file_or_err!(tasks, TODO_FILE_NAME);
