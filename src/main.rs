26580ab8ddf8d6efc6acf40d0754378fd01957d0
eropsyren <eropsyren@gmail.com>
2019-07-26 18:52:42 +0200

    improve help messages

--- src/main.rs
+++ src/main.rs
@@ -87,7 +87,7 @@ fn main() {
                 ),
         )
         .subcommand(
-            SubCommand::with_name("remove").about("Remove a task").arg(
+            SubCommand::with_name("remove").about("Removes a task").arg(
                 Arg::with_name("id")
                     .value_name("ID")
                     .help("Task's id to be marked as discarded")
@@ -96,7 +96,7 @@ fn main() {
         )
         .subcommand(
             SubCommand::with_name("edit")
-                .about("Edit a task description")
+                .about("Edit sa task description")
                 .arg(
                     Arg::with_name("id")
                         .value_name("ID")
